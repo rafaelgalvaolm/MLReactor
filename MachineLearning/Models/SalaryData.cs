13e47ad6707257c882b1f5c09433802f0451d727
Rafael Galvao <rafaelgalvaolm.dev@gmail.com>
2024-02-26 17:19:19 -0300

    config inicial

---
+++ MachineLearning/Models/SalaryData.cs
@@ -0,0 +1,10 @@
+﻿namespace MLNetReactor.MachineLearning.Models
+{
+    public class SalaryData
+    {
+        public float Age { get; set; }
+        public string? Workclass { get; set; }
+        public float EducationNum { get; set; }
+        public string? MaritalStatus { get; set; }
+    }
+}
---
+++ MachineLearning/Services/PredictionService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ML;
+using MLNetReactor.MachineLearning.Models;
+using static MLNetReactor.MLModel;
+
+namespace MLNetReactor.MachineLearning.Services
+{
+    public class PredictionService
+    {
+
+        private readonly ITransformer _model;
+        private readonly MLContext _mlContext = new MLContext();
+
+        public PredictionService(string modelPath)
+        {
+            // Load the trained model
+            DataViewSchema modelSchema;
+            _model = _mlContext.Model.Load(modelPath, out modelSchema);
+        }
+
+        public SalaryPrediction Predict(SalaryData inputData)
+        {
+            var predEngine = _mlContext.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(_model);
+            return predEngine.Predict(inputData);
+        }
+    }
+}
